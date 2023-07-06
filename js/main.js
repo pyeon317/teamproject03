29b9e1473370cafe8c7688b91c0ec5f86a4b295a
pyeon317 <pyeon317@gmail.com>
2023-07-06 15:51:03 +0900

    main02 jinung

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var close = document.querySelector('.btn_typeA1');
+var popup = document.querySelector('.popup');
+close.onclick = function(){
+    console.log(popup);
+    popup.innerHTML = "";
+
+}
